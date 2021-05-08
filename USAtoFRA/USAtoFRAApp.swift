fd35cb2d89f37a901775555087a23f787556a473
Thomas <contact@thomas-george.com>
2021-05-08 16:02:22 +0200

    Initial Commit

---
+++ USAtoFRA/USAtoFRAApp.swift
@@ -0,0 +1,17 @@
+//
+//  USAtoFRAApp.swift
+//  USAtoFRA
+//
+//  Created by Thomas George on 08/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct USAtoFRAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
